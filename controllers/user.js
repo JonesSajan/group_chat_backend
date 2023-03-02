61ec21a46da5ee319267d4e061e9ae240d72d825
Jones <Jonessajan3000@gmail.com>
2023-03-02 21:52:34 +0530

    login functionality added in backend

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,12 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const saltrounds = 10;
+const jwt=require('jsonwebtoken')
+
+const generateToken=(id,name)=>{
+    return jwt.sign({id:id,name:name},'8770903047')
+}
+
 
 exports.setUser = (req, res, next) => {
   // console.log("add user called")
@@ -30,3 +36,26 @@ exports.setUser = (req, res, next) => {
       });
   });
 };
+
+exports.loginUser = async (req, res, next) => {
+    try {
+        console.log("GET BY ID CALLED");
+        email = req.body.email;
+        password = req.body.password;
+      result = await User.findAll({ where: { email: email } });
+
+      console.log(result[0].dataValues.password)
+      if(result){
+       const response= await bcrypt.compare(password,result[0].dataValues.password) ;
+    //    console.log(result[0].name);    
+       response?res.status(200).json({msg:"Login Successfull",token:generateToken(result[0].id,result[0].name)}):res.status(401).json("incorrect password");
+      }
+      
+    } catch (err) {
+      console.log(err);
+      res.status(404).send({msg:"User don't exist"})
+    }
+    
+
+  };
+
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,8 @@ const userController = require('../controllers/user')
 const router = express.Router();
 
 router.post('/adduser',userController.setUser)
+router.post('/loginuser',userController.loginUser)
+
 
 
 module.exports =router
