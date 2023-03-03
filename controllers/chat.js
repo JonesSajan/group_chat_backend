cd4290150a8796e454819745b1e6cbd869606502
Jones <Jonessajan3000@gmail.com>
2023-03-03 13:06:47 +0530

    api for getting messages from backend enabled

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,6 +1,20 @@
+const Chat = require('../models/chat')
 
 exports.sendChat = async (req,res,next)=>{
+    try{
     const message = req.body.message
     const result = await req.user.createChat({message:message})
     res.status(200).json(result)
+}catch(err){
+    console.log(err)
+}
+}
+
+exports.getChat = async (req,res,next)=>{
+    try{
+    const result = await Chat.findAll()
+    res.status(200).json(result)
+}catch(err){
+    console.log(err)
+}
 }
